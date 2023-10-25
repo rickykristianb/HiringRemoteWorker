10bebc77955dd40f8394e688e1b0eb6c1ca2776b
Ricky Kristian B <ricky.kristianb@gmail.com>
2023-10-25 15:17:57 +0700

    fix userApi models

--- userApi/models.py
+++ userApi/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     username = models.CharField(max_length=255, unique=True, null=False, blank=False)
-    email = models.EmailField(max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, unique=True, null=False, blank=False)
     location = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=14, unique=True, null=True, blank=True)
     short_intro = models.CharField(max_length=255, null=True, blank=True)
@@ -21,84 +21,23 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     skills = models.ManyToManyField("Skills", blank=True)
     languages = models.ManyToManyField("Language", blank=True)
-    employment_type = models.ManyToManyField("EmploymentType", blank=True)
-    rating_received = models.ManyToManyField('Profile', through="UserRate", related_name='rated_profiles')
+    employment_type = models.CharField(max_length=50, null=True, blank=True)
+    user_type = models.CharField(max_length=20, null=True, blank=True)
     rate_total = models.FloatField(default=0, null=True, blank=True)
     rate_ratio = models.FloatField(default=0, null=True, blank=True)
-    # user_rate_id integer
-    # user_type_id integer
-    # experience_id integer
-    # education_id integer
-    # portofolio_id integer
-    # expected_salary_id integer
-    # networking_id integer
-    # working_history_id integer
-    # job_posted_id integer
 
     def __str__(self) -> str:
         return self.username
     
-    # def save_rate_field(self):
-    #     with connection.cursor() as cursor:
-    #         cursor.execute("SELECT SUM(rate_value) FROM userApi_userrate WHERE to_user_id=%s", [str(self.id).replace("-","")])
-    #         sum_rate_value = cursor.fetchone()
-    #         self.rate_total = sum_rate_value[0]
-    #         self.save()
-
-
-
-
-        # with connection.cursor() as cursor:
-        #     # cursor.execute(
-        #     #     """
-        #     #     SELECT rate_value
-        #     #     FROM userApi_UserRate 
-        #     #     WHERE to_user_id = %s
-        #     #     """, [str(self.id)])
-            
-        #     # result = cursor.fetchall()
-        #     # print("ID", self.id)
-        #     # print("RESULT: ", result)
-        #     # cursor.execute("UPDATE bar SET foo = 1 WHERE baz = %s", [self.baz])
-        #     cursor.execute(
-        #         """
-        #     SELECT SUM(CAST(rate_value AS INTEGER)) AS total_ratings
-        #     FROM userApi_userrate
-        #     WHERE to_user_id = %s
-        #     """, [str(self.id)])
-        #     test = cursor.fetchone()
-
-        #     self.rate_total = test
-        #     self.save()
-
-        #     print(self.id)
-        #     print("result", test)
-        # # total_ratings = UserRate.objects.filter(to_user=self).aggregate(total_ratings=Sum(Cast('rate_value', models.IntegerField())))["total_ratings"]
-        # # print(total_ratings)
-        # self.rate_total = total_ratings
-        # self.save()
-
 
 class UserRate(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     from_user = models.ForeignKey(Profile, related_name="given_rating", null=True, on_delete=models.CASCADE)
     to_user = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
-
-    RATE_CHOICES = [
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4),
-        (5, 5)
-    ]
-    rate_value = models.IntegerField(choices=RATE_CHOICES, default=5)
+    rate_value = models.IntegerField(default=5)
 
     def __str__(self) -> str:
         return f"{self.from_user}, {self.to_user}" 
-    
-    # def save(self):
-    #     super(UserRate, self).save(self)
-    #     self.to_user.save_rate_field()
     
     class Meta:
         unique_together = ["from_user", "to_user"]
@@ -106,26 +45,8 @@ class UserRate(models.Model):
     
 class Language(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
-    # user = models.ForeignKey(Profile, related_name='languages', on_delete=models.CASCADE, blank=True)
-    LANGUAGE_CHOICES = [
-        ("Indonesia", "INDONESIA"),
-        ("English", "ENGLISH"),
-    ]
-    language = models.CharField(max_length=20, default="ID", choices=LANGUAGE_CHOICES)
-
-    NATIVE = "native"
-    EXPERT = "expert"
-    FLUENT = "fluent"
-    INTERMEDIATE = "intermediate"
-    BEGINNER = "beginner"
-    PROFICIENCY_CHOICES = [
-        (NATIVE, "Native"),
-        (EXPERT, "Expert"),
-        (FLUENT, "Fluent"),
-        (INTERMEDIATE, "Intermediate"),
-        (BEGINNER, "Beginner")
-    ]
-    proficiency = models.CharField(max_length=20, choices=PROFICIENCY_CHOICES, default=NATIVE)
+    language = models.CharField(max_length=20, null=True, blank=True)
+    proficiency = models.CharField(max_length=20, null=True, blank=True)
     
     def __str__(self) -> str:
         return f"{self.language}, {self.proficiency}"
@@ -134,81 +55,17 @@ class Language(models.Model):
 class Skills(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     skill_name = models.CharField(max_length=20, null=True, blank=True)
-
-    NOVICE = "novice"
-    BEGINNER = "beginner"
-    ADVANCE_BEGINNER = "advance_beginner"
-    COMPETENT = "competent"
-    PROFICIENT = "proficient"
-    EXPERT = "expert"
-    SKILL_LEVEL_CHOICES = [
-        (NOVICE, "Novice"), 
-        (BEGINNER, "Beginner"), 
-        (ADVANCE_BEGINNER, "Advanced Beginner"),
-        (COMPETENT, "Competent"),
-        (PROFICIENT, "Proficient"),
-        (EXPERT, "Expert")
-    ]
-    skill_level = models.CharField(max_length=50, choices=SKILL_LEVEL_CHOICES, default=NOVICE)
+    skill_level = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.skill_name}, {self.skill_level}"
     
     
-class EmploymentType(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    FULL_TIME_CONTRACT = "fulltime_contract"
-    FULL_TIME_PERMANENT = "fulltime_permanent"
-    PART_TIME_CONTRACT = "parttime_contract"
-    PART_TIME_PERMANENT = "parttime_permanent"
-    TYPE_CHOICES = [
-        (FULL_TIME_CONTRACT, "Full-time/Contract"),
-        (FULL_TIME_PERMANENT, "Full-time/Permanent"),
-        (PART_TIME_CONTRACT, "Part-time/Contract"),
-        (PART_TIME_PERMANENT, "Part-time/Permanent")
-    ]
-    type_name = models.CharField(max_length=50, choices=TYPE_CHOICES, default=FULL_TIME_CONTRACT)
-
-    @property
-    def get_type_name(self):
-        return dict(self.TYPE_CHOICES).get(int(self.type_name))
-
-    def __str__(self) -> str:
-        return f"{self.get_type_name}, {self.type_name}"
-
-
-
-
-
-class UserType(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    user = models.OneToOneField(User, null=False, blank=False, default="Personal", on_delete=models.CASCADE)
-
-    PERSONAL = "personal"
-    COMPANY = "company"
-    TYPE_CHOICES = [
-        (PERSONAL, "Personal"),
-        (COMPANY, "Company")
-    ]
-    type_name = models.CharField(max_length=20, choices=TYPE_CHOICES, default=PERSONAL)
-
-    def __str__(self) -> str:
-        return f"{self.user}, {self.type_name}"
-    
 class Education(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     user = models.ForeignKey(Profile, null=False, blank=False, default="Personal", on_delete=models.CASCADE)
     major = models.CharField(max_length=50, null=True, blank=True)
-
-    DIPLOMA = "diploma"
-    BACHELOR = "bachelor"
-    POSTGRADUATE = "postgraduate"
-    DEGREE_CHOICES =[
-        (DIPLOMA, "Diploma"),
-        (BACHELOR, "Bachelor"),
-        (POSTGRADUATE, "Postgraduate")
-    ]
-    degrees = models.CharField(max_length=20, choices=DEGREE_CHOICES, default=DIPLOMA)
+    degrees = models.CharField(max_length=20, null=True, blank=True)
     school_name = models.CharField(max_length=100, null=True, blank=True)
     start_year = models.DateField()
     end_year = models.DateField()
@@ -219,60 +76,36 @@ class Education(models.Model):
 class ExpectedSalary(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     user = models.OneToOneField(Profile, null=False, blank=False, default="Personal", on_delete=models.CASCADE)
-
-    RUPIAH = "rp"
-    USDOLLAR = "usd"
-    CURRENCY_CHOICES = [
-        (RUPIAH, "RP"),
-        (USDOLLAR, "USD")
-    ]
-    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUPIAH)
-
+    currency = models.CharField(max_length=3, null=True, blank=True)
     nominal = models.FloatField()
-    HOURLY = "hourly"
-    MONTHLY = "monthly"
-    YEARLY = "yearly"
-    PAID_PERIOD_CHOICES = [
-        (HOURLY, "Hourly"),
-        (MONTHLY, "Honthly"),
-        (YEARLY, "Yearly")
-    ]
-    paid_period = models.CharField(max_length=20, choices=PAID_PERIOD_CHOICES, default=HOURLY)
+    paid_period = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self) -> str:
         return str(self.nominal)
     
 class Experience(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    user = models.ForeignKey(Profile, null=False, blank=False, default="Personal", on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, null=False, blank=False, on_delete=models.CASCADE)
     job_name = models.CharField(max_length=100, null=False, blank=False)
     company_name = models.CharField(max_length=100, null=True, blank=True)
     start_date = models.DateField()
     end_date = models.DateField()
     details = models.TextField(max_length=600, null=True, blank=True)
-    total_exp = models.FloatField()
+    total_exp = models.FloatField(null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.user}, {self.company_name}, {self.start_date}, {self.end_date}"
+    
     
 class Networking(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     user = models.ForeignKey(Profile, null=False, blank=False, default="Personal", on_delete=models.CASCADE)
     networking_link = models.CharField(max_length=100, null=False, blank=False)
-    NETWORKING_CHOICES = [
-        (1, "LinkedIn"),
-        (2, "Facebook"),
-        (3, "Instagram"),
-        (4, "Twitter")
-    ]
-    networking_name = models.CharField(max_length=20, choices=NETWORKING_CHOICES, null=False, blank=False, default=1)
+    networking_name = models.CharField(max_length=20, null=True, blank=True)
 
-    @property
-    def get_network_name(self):
-        return dict(self.NETWORKING_CHOICES).get(self.networking_name)
 
     def __str__(self) -> str:
-        return f"{self.user}, {self.get_network_name}"
+        return f"{self.user}, {self.networking_name}, {self.networking_link}"
     
     class Meta:
         unique_together = ["user", "networking_name"]
@@ -281,18 +114,7 @@ class Portfolio(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     user = models.ForeignKey(Profile, null=False, blank=False, default="Personal", on_delete=models.CASCADE)
     portfolio_link = models.CharField(max_length=100, null=False, blank=False)
-
-    GITHUB = "github"
-    LINKEDIN = "linkedin"
-    PERSONAL_WEBSITE = "personal_website"
-    OTHER_LINK = "other_link"
-    PORTFOLIO_CHOICES = [
-        (GITHUB, "Github"),
-        (LINKEDIN, "LinkedIn"),
-        (PERSONAL_WEBSITE, "Personal Website"),
-        (OTHER_LINK, "Other Link")
-    ]
-    portfolio_name = models.CharField(max_length=20, choices=PORTFOLIO_CHOICES, null=False, blank=False, default=1)
+    portfolio_name = models.CharField(max_length=20, null=True, blank=True)
 
     @property
     def get_portfolio_name(self):
